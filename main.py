791c7c606b8705589f68f2628be68f963be18092
nataliemeow <razorlovesbaba@tuta.io>
2025-08-13 02:04:37 +0300

    use OutJson in main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from parser import Parser
-from parser.ext import Include, Aliases
+from parser.ext import Include, Aliases, OutJson
 from sys import stdin
 
-print(Parser.use(Include, Aliases)(stdin.read()).parse().emit())
+print(Parser.use(Include, Aliases, OutJson)(stdin.read()).parse().emit())
