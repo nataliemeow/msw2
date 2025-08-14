a155377c9ec5709327aea783921a3cb5e6b337e7
nataliemeow <ataberkzxc@gmail.com>
2025-08-14 14:51:16 +0300

    add ShortLoad and HexLiterals to main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from parser import Parser
-from parser.ext import Include, Aliases, OutJson
+from parser.ext import Include, Aliases, OutJson, ShortLoad, HexLiterals
 import sys
 
 with open(sys.argv[1]) as f:
 	print(
-		Parser.use(Include, Aliases, OutJson)(
+		Parser.use(Include, Aliases, ShortLoad, OutJson, HexLiterals)(
 			# i did not want to have to do this but my silly ass mixin thing necessitates it i think
 			f.read(), path=sys.argv[1] # type: ignore
 		).parse().emit()
