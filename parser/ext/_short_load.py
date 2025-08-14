7881d45490a476a1f5056d56605f260537600f86
nataliemeow <ataberkzxc@gmail.com>
2025-08-14 14:47:10 +0300

    add ShortLoad

---
+++ parser/ext/_short_load.py
@@ -0,0 +1,16 @@
+from parser import Parser, StringNode, CallListNode
+from typing import cast
+import os.path
+
+def ShortLoad(base: type[Parser]):
+  '''Replace `.VAR` with `(get VAR)`.'''
+
+  class _ShortLoad(base):
+    def _parse_node(self):
+      node = super()._parse_node()
+      if not isinstance(node, StringNode): return node
+      if not node.startswith('.'): return node
+      
+      return CallListNode([StringNode('load'), StringNode(node[1:])])
+
+  return _ShortLoad
